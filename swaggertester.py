4f00efc06e783e3bac5b392e00c88fc0aa51b9a1
olipratt <olipratt@users.noreply.github.com>
2017-03-06 20:31:26 +0000

    Started to expand API

--- swaggertester.py
+++ swaggertester.py
@@ -10,16 +10,60 @@ logging.getLogger("pyswagger").setLevel(logging.WARNING)
 log = logging.getLogger(__name__)
 
 
+class ParameterTemplate:
+
+    def __init__(self, parameter):
+        assert parameter.type is not None
+        self._type = parameter.type
+        assert parameter.name is not None
+        self._name = parameter.name
+
+
+class EndpointCollection:
+
+    operations = ["get", "put"]
+
+    def __init__(self, schema_path):
+        log.debug("Creating new endpoint collection for: %r", schema_path)
+        self._schema_path = schema_path
+
+        self._app = App.create(schema_path)
+
+        self._paths = self._app.root.paths.keys()
+        log.debug("Found paths as: %s", self._paths)
+
+        self._expanded_paths = {}
+        for path in self._paths:
+            self._expanded_paths[path] = self._expand_path(path)
+
+    def _expand_path(self, path):
+        log.debug("Expanding path: %r", path)
+
+        operations_map = {}
+        for operation_name in self.operations:
+            log.debug("Accessing operation: %s", operation_name)
+            operation_params = {}
+            operation = getattr(self._app.root.paths[path], operation_name)
+            if operation is not None:
+                log.debug("Have operation")
+                operations_map[operation_name] = operation_params
+                for parameter in operation.parameters:
+                    log.debug("Handling parameter: %r", parameter.name)
+
+                    if parameter.schema is None:
+                        log.debug("Fully defined parameter")
+                        param_template = ParameterTemplate(parameter)
+                        operation_params[parameter.name] = param_template
+                    else:
+                        log.debug("Schema defined parameter")
+                        log.warning("SKIPPING SCHEMA PARAM - NOT IMPLEMENTED")
+
+        log.debug("Expanded path as: %r", operations_map)
+
+
 def main(schema_path):
-    # Load Swagger schema.
-    app = App.create(schema_path)
-
-    log.debug("App paths: %r", app.root.paths)
-
-    paths = app.root.paths.keys()
-    log.info("Found paths as: %s", paths)
-
-    # client = Client(Security(app))
+    endpoints = EndpointCollection(schema_path)
+    log.debug("Expanded endpoints as: %r", endpoints)
 
 
 if __name__ == '__main__':
