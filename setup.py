457b14705a17a0f8b351e1199b3b0c34a6c61aff
Oliver Pratt <olipratt@users.noreply.github.com>
2017-04-13 22:37:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'swagger-conformance',
+  packages = ['swaggerconformance'],
+  install_requires=['hypothesis', 'pyswagger', 'requests'],
+  version = '0.1',
+  description = 'Tool for testing whether your API conforms to it's swagger specification',
+  author = 'Oli Pratt',
+  author_email = 'olipratt@users.noreply.github.com',
+  url = 'https://github.com/olipratt/swagger-conformance',
+  download_url = 'https://github.com/olipratt/swagger-conformance/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['swagger', 'testing', 'OpenAPI', 'hypothesis'],
+  license='MIT',
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Testing',
+     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
