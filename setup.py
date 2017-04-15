50889a83da46234e841ffb11752232163ffbd73a
olipratt <olipratt@users.noreply.github.com>
2017-04-15 15:21:33 +0100

    Indicate package does not depend on OS

--- setup.py
+++ setup.py
@@ -38,11 +38,12 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Testing',
     ],
 )
